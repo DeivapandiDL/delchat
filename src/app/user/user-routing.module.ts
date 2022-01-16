ec1105834cf35a723e57a0307a4a01b09f2ba86a
DeivapandiDL <thamdeva@gmail.com>
2022-01-16 11:06:01 +0530

    first chat commit

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserModule } from './user/user.module';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-  {
-    path:'',
-    loadChildren: () => import('./user/user.module').then( m => m.UserModule)
-  }
+    {path:'', component:UserComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class userRoutingModule { }
